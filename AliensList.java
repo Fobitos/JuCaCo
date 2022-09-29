f82416b546eb46c501ecc4522816955451c72c87
Juan Castro <103339814+Fobitos@users.noreply.github.com>
2022-09-28 22:02:47 -0300

    Update AliensList.java

--- AliensList.java
+++ AliensList.java
@@ -1,5 +1,3 @@
-package codes;
-
 public class AliensList {
 	
 	private Alien[] aliens;
@@ -7,9 +5,63 @@ public class AliensList {
 	private int max;
 	
 	public AliensList(int max) {
-		this.max = max;
 		aliens = new Alien[max];
 		cant = 0;
+		this.max = max;
+	}
+	
+	public boolean add(Alien a) {
+		if(cant < max) {
+			aliens[cant] = a;
+			cant++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public int index(Alien a) {
+		int i;
+		for(i = 0; i < cant; i++) {
+			if(aliens[i] == a) {
+				break;
+			}
+		}
+		if(i == cant) {
+			return -1;
+		}
+		else { 
+			return i;
+		}
+	}
+	
+	public boolean delete(Alien a) {
+		int i;
+		for(i = 0; i < cant; i++) {
+			if(aliens[i] == a) {
+				break;
+			}
+		}
+		if(i == cant) {
+			return false;
+		}
+		else {
+			for(int j = i; j < cant; j++) {
+				aliens[j] = aliens[j + 1];
+			}
+			cant--;
+			return true;
+		}
+	}
+	
+	public Alien getAlien(int i) {
+		if(i < cant) {
+			return aliens[i];
+		}
+		else { 
+			return null;
+		}
 	}
 
 	public int getCant() {
@@ -27,5 +79,4 @@ public class AliensList {
 	public void setMax(int max) {
 		this.max = max;
 	}
-
 }
